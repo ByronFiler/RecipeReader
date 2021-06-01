1d79585c49b9a4bd5a8bef2e92910aad0609230d
Byron Filer <byronfiler348@gmail.com>
2021-06-01 17:05:38 +0100

    update

--- reader.js
+++ reader.js
@@ -1,6 +1,8 @@
-(function() {
+const domains = [
+    JoshuaWeissman, EthanChlebowski
+];
 
-    console.log("here");
+(function() {
 
     for (let i = 0; i < document.scripts.length; i++) {
         if (document.scripts.item(i).text.includes("joshuaweissman.com") && document.scripts.item(i).text.startsWith("{")) {
@@ -8,19 +10,20 @@
                 .description
                 .split("\n")
                 .forEach(line => {
-                    if (line.includes("joshuaweissman.com")) {
-                        let url = line.match(/\bhttps?:\/\/\S+/gi)[0];
+                    domains.forEach(domain => {
+                        if (line.includes(domain[0].getDomain())) {
+                            let url = line.match(/\bhttps?:\/\/\S+/gi)[0];
 
-                        fetch(url, {
-                            headers: {
-                                "Access-Control-Allow-Origin": "*"
-                            }
-                        }).then(
+                            fetch(url, {
+                                headers: {
+                                    "Access-Control-Allow-Origin": "*"
+                                }
+                            }).then(
                                 function(response) {
                                     if (response.status === 200) {
 
+                                        let processedString = response.text().then(str => domains[1].parse(str));
 
-                                        let processedString = response.text().then(joshuaweissmanParser);
                                         let description = document.getElementsByClassName("content style-scope ytd-video-secondary-info-renderer").item(0);
 
                                         processedString.then(strings => {
@@ -38,38 +41,80 @@
                                     }
                                 }
                             );
-                    }
+                        }
+                    })
             });
         }
     }
 
 })();
 
-function joshuaweissmanParser(HTML) {
+class site {
 
-    let processedString = [];
+    constructor() {
+        if (new.target === site) {
+            throw new TypeError("Cannot construct site instance, class is abstract.")
+        }
+    }
 
-    HTML.split("<strong>")
-        .forEach(section => {
+    parse(HTML) {}
 
-            if (section.split("</strong>")[0].endsWith(":")) {
-                processedString.push( "<br><strong>" + section.split("</strong>")[0] + "</strong><br>")
-
-                section.split("<p class=").forEach(line => {
-                    if (!line.split("</p>")[0].split(">")[1].includes("<")) {
-                        processedString.push(line.split("</p>")[0].split(">")[1] + "<br>");
-                    }
-                })
-
-            }
-
-        });
-
-    return processedString.reverse();
-
-
-
+    getDomain() {}
 
 }
+
+class JoshuaWeissman extends site {
+
+    domain = "joshuaweissman.com"
+
+    getDomain() {
+        return this.domain;
+    }
+
+    parse(HTML) {
+        let processedString = [];
+
+        HTML.split("<strong>")
+            .forEach(section => {
+
+                if (section.split("</strong>")[0].endsWith(":")) {
+                    processedString.push( "<br><strong>" + section.split("</strong>")[0] + "</strong><br>")
+
+                    section.split("<p class=").forEach(line => {
+                        if (!line.split("</p>")[0].split(">")[1].includes("<")) {
+                            processedString.push(line.split("</p>")[0].split(">")[1] + "<br>");
+                        }
+                    })
+
+                }
+
+            });
+
+        return processedString.reverse();
+    }
+
+}
+
+class EthanChlebowski extends site {
+
+    domain = "ethanchlebowski.com";
+
+    getDomain() {
+        return this.domain;
+    }
+
+    parse(HTML) {
+        let processedString = [];
+
+        HTML.split("<label>").forEach(label => {
+            console.log(label.split("<span>")[0].split("</span>"));
+        })
+
+        return processedString.reverse();
+    }
+
+}
+
+
 
 
