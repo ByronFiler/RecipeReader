3933f48c7853d8362d336f54dde4dcb1ce46e4de
Byron Filer <byronfiler348@gmail.com>
2021-06-01 19:37:58 +0100

    refactor and support for Ethan Chlebloski

--- reader.js
+++ reader.js
@@ -1,28 +1,127 @@
-const domains = [
-    JoshuaWeissman, EthanChlebowski
-];
-
 (function() {
+    class Site {
+
+        constructor() {
+            if (new.target === Site) {
+                throw new TypeError("Cannot construct site instance, class is abstract.")
+            }
+        }
+
+        parse(HTML) {}
+
+        getDomain() {}
+
+    }
+
+    class JoshuaWeissman extends Site{
+
+        domain = "joshuaweissman.com"
+
+        constructor() {
+            super();
+        }
+
+        getDomain() {
+            return this.domain;
+        }
+
+        parse(HTML) {
+            let processedString = [];
+
+            HTML.split("<strong>")
+                .forEach(section => {
+
+                    if (section.split("</strong>")[0].endsWith(":")) {
+                        processedString.push( "<br><strong>" + section.split("</strong>")[0] + "</strong><br>")
+
+                        section.split("<p class=").forEach(line => {
+                            if (!line.split("</p>")[0].split(">")[1].includes("<")) {
+                                processedString.push(line.split("</p>")[0].split(">")[1] + "<br>");
+                            }
+                        })
+
+                    }
+
+                });
+
+            return processedString.reverse();
+        }
+
+    }
+
+    class EthanChlebowski extends Site {
+
+        domain = "ethanchlebowski.com";
+
+        constructor() {
+            super();
+        }
+
+        getDomain() {
+            return this.domain;
+        }
+
+        parse(HTML) {
+            let processedString = [];
+
+            HTML
+                .split("<div class=\"ccm-section\">")
+                .slice(1)
+                .forEach(section => {
+
+
+                    processedString.push("<br><strong>" + (section.split("<class=\"ccm-section-title\">")[0].split("</div>")[0].split(">")[1]) + "</strong><br>")
+
+                    // console.log("section title: " + section.split("<class=\"ccm-section-title\">")[0].split("</div>")[0].split(">")[1])
+
+                    section.split("<li>").splice(1).forEach(ingredient => {
+
+                        ingredient = ingredient.split("</li>")[0];
+
+                        if (ingredient.includes("<span>")) {
+                            processedString.push(ingredient.split("<span>")[1].split("</span>")[0] + "<br>")
+                            // console.log("ingredient: " + ingredient.split("<span>")[1].split("</span>")[0]);
+                        } else {
+                            processedString.push("<i>" + ingredient + "</i><br>")
+                            // console.log("instructor: " + ingredient);
+                        }
+                    });
+
+            });
+
+            console.log(processedString);
+
+            return processedString.reverse();
+        }
+
+    }
+
+    const domains = [
+        new JoshuaWeissman(), new EthanChlebowski()
+    ];
+
+    console.log("")
 
     for (let i = 0; i < document.scripts.length; i++) {
-        if (document.scripts.item(i).text.includes("joshuaweissman.com") && document.scripts.item(i).text.startsWith("{")) {
-            JSON.parse(document.scripts.item(i).text)
-                .description
-                .split("\n")
-                .forEach(line => {
-                    domains.forEach(domain => {
-                        if (line.includes(domain[0].getDomain())) {
+
+        domains.forEach(domain => {
+            if (document.scripts.item(i).text.includes(domain.getDomain()) && document.scripts.item(i).text.startsWith("{")) {
+                JSON.parse(document.scripts.item(i).text)
+                    .description
+                    .split("\n")
+                    .forEach(line => {
+                        if (line.includes(domain.getDomain())) {
                             let url = line.match(/\bhttps?:\/\/\S+/gi)[0];
 
                             fetch(url, {
-                                headers: {
-                                    "Access-Control-Allow-Origin": "*"
-                                }
+                                // headers: {
+                                //     "Access-Control-Allow-Origin": "*"
+                                // }
                             }).then(
-                                function(response) {
+                                function (response) {
                                     if (response.status === 200) {
 
-                                        let processedString = response.text().then(str => domains[1].parse(str));
+                                        let processedString = response.text().then(domain.parse);
 
                                         let description = document.getElementsByClassName("content style-scope ytd-video-secondary-info-renderer").item(0);
 
@@ -42,78 +141,13 @@ const domains = [
                                 }
                             );
                         }
-                    })
-            });
-        }
+
+                    });
+            }
+        });
     }
 
 })();
-
-class site {
-
-    constructor() {
-        if (new.target === site) {
-            throw new TypeError("Cannot construct site instance, class is abstract.")
-        }
-    }
-
-    parse(HTML) {}
-
-    getDomain() {}
-
-}
-
-class JoshuaWeissman extends site {
-
-    domain = "joshuaweissman.com"
-
-    getDomain() {
-        return this.domain;
-    }
-
-    parse(HTML) {
-        let processedString = [];
-
-        HTML.split("<strong>")
-            .forEach(section => {
-
-                if (section.split("</strong>")[0].endsWith(":")) {
-                    processedString.push( "<br><strong>" + section.split("</strong>")[0] + "</strong><br>")
-
-                    section.split("<p class=").forEach(line => {
-                        if (!line.split("</p>")[0].split(">")[1].includes("<")) {
-                            processedString.push(line.split("</p>")[0].split(">")[1] + "<br>");
-                        }
-                    })
-
-                }
-
-            });
-
-        return processedString.reverse();
-    }
-
-}
-
-class EthanChlebowski extends site {
-
-    domain = "ethanchlebowski.com";
-
-    getDomain() {
-        return this.domain;
-    }
-
-    parse(HTML) {
-        let processedString = [];
-
-        HTML.split("<label>").forEach(label => {
-            console.log(label.split("<span>")[0].split("</span>"));
-        })
-
-        return processedString.reverse();
-    }
-
-}
 
 
 
