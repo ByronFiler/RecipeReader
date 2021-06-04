d77d8d198c8a7c9a488572193ea652a0ebd6a3b3
Byron Filer <byronfiler348@gmail.com>
2021-06-04 21:57:49 +0100

    updated for ethan chlebloski

--- sites/EthanChlebowski.js
+++ sites/EthanChlebowski.js
@@ -1,3 +1,17 @@
+/**
+ * Ethan Chlebowski
+ * Relatively simple and consistent website
+ * >=1 Method Blocks
+ * >=1 Instruction Blocks
+ *
+ * Links
+ * Channel: https://www.youtube.com/channel/UCDq5v10l4wkV5-ZBIJJFbzQ
+ * Site:    https://www.ethanchlebowski.com/
+ *
+ * Example
+ * Video:   https://www.youtube.com/watch?v=KdbWEjWnXPA
+ * Recipe:  https://www.ethanchlebowski.com/cooking-techniques-recipes/green-chile-chicken-enchiladas
+ */
 class EthanChlebowski extends Site {
 
     #domain = "ethanchlebowski.com";
@@ -11,27 +25,35 @@ class EthanChlebowski extends Site {
     }
 
     parse(HTML) {
-        let processedString = [];
+        let blocks = [];
 
         HTML
             .split("<div class=\"ccm-section\">")
             .slice(1)
             .forEach(section => {
-                processedString.push("<br><strong>" + (section.split("<class=\"ccm-section-title\">")[0].split("</div>")[0].split(">")[1]) + "</strong><br>")
+
+                let block = section.split("<ul").length > section.split("<ol").length ?
+                    new IngredientBlock() : new MethodBlock();
+
+                block.setTitle(section.split("<class=\"ccm-section-title\">")[0].split("</div>")[0].split(">")[1]);
 
                 section
                     .split("<li>")
                     .splice(1)
                     .map(ingredient => ingredient.split("</li>")[0])
                     .forEach(ingredient => {
-                        processedString.push(ingredient.includes("<span>") ?
-                            ingredient.split("<span>")[1].split("</span>")[0] + "<br>"
-                            : "<i>" + ingredient + "</i><br>");
+                        block.add(
+                            ingredient.includes("<span>") ?
+                                ingredient.split("<span>")[1].split("</span>")[0]
+                                : ingredient
+                        )
                     });
 
+                blocks.push(block);
+
             });
-        
-        return processedString.reverse();
+
+        return blocks.reverse();
     }
 
 }
